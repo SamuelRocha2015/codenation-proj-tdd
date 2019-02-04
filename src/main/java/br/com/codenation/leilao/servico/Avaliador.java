aaf0950f2d195cf948849c564652ccceb400ad6c
Samuel Rocha <samuelrodriguessr@hotmail.com>
2019-02-04 01:08:40 -0300

    starater project

---
+++ src/main/java/br/com/codenation/leilao/servico/Avaliador.java
@@ -0,0 +1,30 @@
+package br.com.codenation.leilao.servico;
+
+import br.com.codenation.leilao.models.Lance;
+import br.com.codenation.leilao.models.Leilao;
+
+public class Avaliador {
+    private double maiorDeTodos = Double.NEGATIVE_INFINITY;
+    private double menorDeTodos = Double.POSITIVE_INFINITY;
+    private double media = 0;
+
+    public void avalia(Leilao leilao) {
+
+
+        double total = 0;
+        for(Lance lance : leilao.getLances()) {
+            if(lance.getValor() > maiorDeTodos) maiorDeTodos = lance.getValor();
+            if(lance.getValor() < menorDeTodos) menorDeTodos = lance.getValor();
+            total += lance.getValor();
+        }
+        if(total == 0) {
+            media = 0;
+            return;
+        }
+        media = total / leilao.getLances().size();
+    }
+
+    public double getMaiorLance() { return maiorDeTodos; }
+    public double getMenorLance() { return menorDeTodos; }
+    public double getMedia() { return media; }
+}
